3117bc8e315690107db765acedc8960975f74e93
polatopola <275188@student.pwr.edu.pl>
2026-04-14 19:51:49 +0200

    Update db.py
    Połączenie z bazą danych

--- api/db.py
+++ api/db.py
@@ -1,1 +1,14 @@
-# TODO
+import psycopg2
+
+DB_HOST = "database"
+DB_NAME = "postgres"
+DB_USER = "postgres"
+DB_PASSWORD = "postgres"
+
+def get_connection():
+    return psycopg2.connect(
+        host=DB_HOST,
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD
+    )# TODO
